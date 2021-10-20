260b6637973b513e6d744883f9fc596ba4b76339
Januario <contact@januariopinto.com>
2021-10-20 10:47:08 +0100

    refactor: extreme makeover

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+// tailwind.config.js
+module.exports = {
+  purge: {
+    enabled: true,
+    content: ['./index.html']
+  },
+   darkMode: 'class', // or 'media' or 'class'
+   theme: {
+     extend: {},
+   },
+   variants: {},
+   plugins: [],
+ }
