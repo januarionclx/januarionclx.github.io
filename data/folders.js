c8ea34a7b0e576c845da7e24380d347767c152ce
januarionclx <januario.personal@gmail.com>
2023-03-10 02:09:06 +0000

    refactor: files

--- data/folders.js
+++ data/folders.js
@@ -8,61 +8,73 @@ export const filesObj = {
       content: [
         {
           type: 'text',
+          title: '/ 2005',
           name: '2005',
           content: '2005.html'
         },
         {
           type: 'text',
           name: '2006',
+          title: '/ 2006',
           content: '2006.html'
         },
         {
           type: 'text',
           name: '2007',
+          title: '/ 2007',
           content: '2007.html'
         },
         {
           type: 'text',
           name: '2009',
+          title: '/ 2009',
           content: '2009.html'
         },
         {
           type: 'text',
           name: '2011',
+          title: '/ 2011',
           content: '2011.html'
         },
         {
           type: 'text',
           name: '2014',
+          title: '/ 2014',
           content: '2014.html'
         },
         {
           type: 'text',
           name: '2015',
+          title: '/ 2015',
           content: '2015.html'
         },
         {
           type: 'text',
           name: '2016',
+          title: '/ 2016',
           content: '2016.html'
         },
         {
           type: 'text',
           name: '2017',
+          title: '/ 2017',
           content: '2017.html'
         },
         {
           type: 'text',
           name: '2018',
+          title: '/ 2018',
           content: '2018.html'
         },
         {
           type: 'text',
           name: '2020',
+          title: '/ 2020',
           content: '2020.html'
         },
         {
           type: 'text',
+          TITLE: '/ NOW',
           name: 'now',
           content: 'now.html'
         },
@@ -78,6 +90,7 @@ export const filesObj = {
     {
       type: 'text',
       name: 'about',
+      title: '/ BIO',
       content: 'about.html'
     },
   ]
